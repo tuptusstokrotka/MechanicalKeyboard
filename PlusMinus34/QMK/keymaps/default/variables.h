a1af0b9ec6bf59399013eb12b0b8090e90955510
Julian <tuptusstokrotka@gmail.com>
2024-08-20 15:15:20 +0200

    spacing

--- PlusMinus34/QMK/keymaps/default/variables.h
+++ PlusMinus34/QMK/keymaps/default/variables.h
@@ -14,14 +14,14 @@
 #define L_SPACE LT(1,KC_ENT)
 #define R_SPACE LT(2,KC_SPC)
 
-#define L_MONITOR SGUI(KC_LEFT)				// Previous Monitor
-#define L_DESKTOP LGUI(LCTL(KC_LEFT))	// Previous Desktop
-#define R_DESKTOP LGUI(LCTL(KC_RGHT))	// Next Desktop
-#define R_MONITOR SGUI(KC_RGHT)				// Next Monitor
+#define L_MONITOR SGUI(KC_LEFT)       // Previous Monitor
+#define L_DESKTOP LGUI(LCTL(KC_LEFT)) // Previous Desktop
+#define R_DESKTOP LGUI(LCTL(KC_RGHT)) // Next Desktop
+#define R_MONITOR SGUI(KC_RGHT)       // Next Monitor
 
 #define L_SNAP LGUI(KC_LEFT)				// Snap Window Left
 #define D_SNAP LGUI(KC_DOWN)				// Snap Window Down
-#define U_SNAP LGUI(KC_UP)					// Snap Window Up
+#define U_SNAP LGUI(KC_UP)          // Snap Window Up
 #define R_SNAP LGUI(KC_RGHT)				// Snap Window Right
 
 // Defines names for use in layer keycodes and the keymap
@@ -34,7 +34,7 @@ enum layer_names {
 
 // Defines the keycodes used by our macros in process_record_user
 enum custom_keycodes {
-	M0 = SAFE_RANGE,
+  M0 = SAFE_RANGE,
   M1,
   M2,
   M3,
